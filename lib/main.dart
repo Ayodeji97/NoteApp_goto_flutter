34a299a4324cf16714ca1ce796cfdf84aeded88b
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-12-06 23:15:29 +0100

    added material app and scaffold widget to app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:note_app_goto/screens/note_list.dart';
+
+
+void main () {
+  
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notekeeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: NoteList(),
+    );
+  }
+}
