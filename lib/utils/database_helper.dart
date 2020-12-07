056e8e0f70d62928c96c3557fd61e9ee1699e2ea
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-12-07 21:53:28 +0100

    complete the sqflite database operation

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
 import 'dart:io';
@@ -58,6 +60,60 @@ class DatabaseHelper {
   void _createDb (Database db, int newVersion) async {
     await db.execute('CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, '
     '$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)');
+  }
+
+  // CRUD operation
+  // Fetch Operation : Get all note objects from database
+  Future <List<Map<String, dynamic>>>getNoteMapList () async {
+      // database reference
+    Database db = await this.database;
+
+   // var result = await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
+    var result = await db.query(noteTable, orderBy: '$colPriority ASC');  // another way to write same things.
+
+    return result;
+
+  }
+
+  // Insert Operation : Insert a note object inside the database
+  Future <int> insertNote (Note note) async {
+      Database db = await this.database;
+
+      var result = await db.insert(noteTable, note.toMap());
+
+      return result;
+  }
+
+  // Update operation : Update a note object and save it to database
+  Future <int> updateNote (Note note) async{
+      Database db = await this.database;
+
+      var result = await db.update(noteTable, note.toMap(), where: '$colId = ?', whereArgs: [note.id]);
+
+      return result;
+  }
+
+  // Delete operation : Delete a note from database
+  Future <int> deleteNote (int id) async {
+      var db = await this.database;
+
+      int result = await db.rawDelete('DELETE FROM $noteTable WHERE $colId = id');
+
+      return result;
+
+  }
+
+  // Get number of note in our database
+
+  Future <int> getCount () async {
+      var db = await this.database;
+
+      List<Map<String, dynamic>> count = await db.rawQuery('SELECT COUNT (*) from $noteTable');
+
+      int result = Sqflite.firstIntValue(count);
+
+      return result;
+
   }
 
 }
